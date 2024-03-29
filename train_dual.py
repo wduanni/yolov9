c392f3e0665f02685cb866a7b7294a260fb6b43d
WDN <wangduanni@126.com>
2024-03-29 21:18:32 +0800

    Update train_dual.py

--- train_dual.py
+++ train_dual.py
@@ -449,7 +449,7 @@ def parse_opt(known=False):
     parser.add_argument('--data', type=str, default='/kaggle/working/yolov9/data/industry.yaml', help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default='/kaggle/working/yolov9/data/hyps/hyp.scratch-high.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=20, help='total training epochs')
-    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs, -1 for autobatch')
+    parser.add_argument('--batch-size', type=int, default=8, help='total batch size for all GPUs, -1 for autobatch')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
     parser.add_argument('--rect', action='store_true', help='rectangular training')
     parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
